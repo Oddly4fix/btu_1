abbbcc733dc75227f343bf00eada13cd3b16a041
Oddly4fix <128570030+Oddly4fix@users.noreply.github.com>
2023-04-25 23:22:55 -0700

    Binary_thermal_unit
    #define NDBUG

---
+++ BTU_object.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <math.h>
+)"Binary_thermal_unit"
+{ int void(register double)
+typename.class(BTU)
+if.double();
+    double.static();
+    double BTU*0.998560
+    while.int;
+    int.static();
+    int BTU();
+    else.double BTU();
+    _Bool BTU();
+    char BTU
+    int y
+    union.register(BTU = y);
+    int(y**,y++,y!!);
+    return 0;
+}
